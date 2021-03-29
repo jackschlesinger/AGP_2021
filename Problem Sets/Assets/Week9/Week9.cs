0b994f49b8319d6ccd349426b624d645899a0924
Jack Schlesinger <jackschlesinger@gmail.com>
2021-03-29 08:54:39 -0400

    Week 9 materials.

---
+++ Problem Sets/Assets/Week9/Week9.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using JetBrains.Annotations;
+using SimpleJSON;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
+
+[ExecuteInEditMode]
+public class Week9 : MonoBehaviour
+{
+
+    public class Node
+    {
+        public List<Node> children;
+        public int value;
+
+        public Node(int value, params Node[] children)
+        {
+            this.value = value;
+            this.children = children.ToList();
+        }
+    }
+    
+    // write a function that returns true if a tree contains a number, false if it doesn't.
+    public bool ContainsNumber(Node root, int number)
+    {
+        return false;
+    }
+
+    // write a function that returns true if the tree contains duplicates, false if not.
+    public bool ContainsDuplicates(Node root)
+    {
+        return false;
+    }
+
+    // write a function to add a new node to the tree as a child of a node w/ value 'toAddTo'
+    // return false if you can't find the node to add to, true if you successfully add it.
+    public bool AddAsChild(Node root, int toAddTo, int numberToAdd)
+    {
+        return false;
+    }
+    
+    // write a function that returns the depth of a number in the tree.  The root should be 0, it's children should
+    // be 1, and so on.  Return -1 if it can't find the number in the tree.
+    public int DepthOfNumber(Node root, int number)
+    {
+        return 0;
+    }
+    
+    // =========================== DON'T EDIT BELOW THIS LINE =========================== //
+
+    public TextMeshProUGUI left, right;
+    
+    private void Update()
+    {
+        var simpleTree = new Node(1, new Node(2, new Node(3), new Node(4)), new Node(5, new Node(6), new Node(7)));
+        var treeWithDuplicates = new Node(1, new Node(2, new Node(3), new Node(1)), new Node(5, new Node(6), new Node(7)));
+
+        
+        left.text =  "Contains Number\n<align=left>\n";
+        left.text += Success(ContainsNumber(simpleTree, 6)) + " returns true correctly.\n";
+        left.text += Success(!ContainsNumber(simpleTree, 8)) + " returns false correctly.\n";
+        left.text +=  "\n</align>Contains Duplicates\n<align=left>\n";
+        left.text += Success(!ContainsDuplicates(simpleTree)) + " correct for tree without duplicates.\n";
+        left.text += Success(ContainsDuplicates(treeWithDuplicates)) + " correct for tree with duplicates.\n";
+
+        var addAsChild1 = new Node(10);
+        right.text = "Add as Child\n<align=left>\n";
+        right.text += Success(!AddAsChild(addAsChild1, 9, 1)) + " add as child works correctly if can't find number.\n";
+
+        var correct = AddAsChild(addAsChild1, 10, 1);
+        right.text += Success(correct && addAsChild1.children[0].value == 1) + " adds child correctly.\n";
+        
+        
+        var depthTree1 = new Node(0, new Node(1));
+        right.text += "\n</align>Depth of Number\n<align=left>\n";
+        right.text += Success(DepthOfNumber(depthTree1, 0) == 0) + " finds depth 0 correctly.\n";
+        right.text += Success(DepthOfNumber(depthTree1, 1) == 1) + " finds depth 1 correctly.\n";
+        right.text += Success(DepthOfNumber(depthTree1, 2) == -1) + " returns correctly when number not in tree.\n";
+    }
+
+    private string Success(bool test)
+    {
+        return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
+    }
+}
---
+++ Projects/PrefixTrie/Assets/SimplePrefixTrie/Scripts/PrefixTrieExample.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class PrefixTrieExample : MonoBehaviour
+{
+    public TextMeshProUGUI display;
+    public TMP_InputField input;
+
+    public TextAsset wordlist;
+
+    private Node root;
+
+    private void Awake()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        
+        root = CreateTree();
+        
+        stopwatch.Stop();
+        Debug.Log("Time creating prefix trie: " + stopwatch.Elapsed.ToString("g"));
+    }
+    
+    private void Update()
+    {
+        if (string.IsNullOrWhiteSpace(input.text))
+        {
+            display.text = "Enter text to see if it's a word in English.";
+        }
+        else
+        {
+            display.text = root.IsWord(input.text) ? "<color=green>Valid" : "<color=red>Invalid";
+        }
+    }
+
+    private Node CreateTree()
+    {
+        var toReturn = new Node();
+
+        foreach (var word in wordlist.text.Split('\n'))
+        {
+            if (string.IsNullOrEmpty(word)) continue;
+
+            toReturn.AddWord(word);
+        }
+
+        return toReturn;
+    }
+}
+
+public class Node
+{
+    private List<Node> children = new List<Node>();
+    private char letter;
+    private bool endsValidWord;
+
+    public void AddWord(string word)
+    {
+        if (word.Length == 0)
+        {
+            endsValidWord = true;
+            return;
+        }
+
+        GetOrAddChild(word[0]).AddWord(word.Substring(1, word.Length - 1));
+    }
+
+    public bool HasChild(char letterToCheck)
+    {
+        /*
+        foreach (var n in children)
+        {
+            if (n.letter == char.ToUpper(letterToCheck))
+                return true;
+        }
+
+        return false;
+        */
+        
+        return children.Any(n => n.letter == char.ToUpper(letterToCheck));
+    }
+
+    public bool AllChildrenAreValidEndOfWord()
+    {
+        /*
+        foreach (var n in children)
+        {
+            if (!n.endsValidWord)
+                return false;
+        }
+
+        return true;
+        */
+        
+        return children.All(n => n.endsValidWord);
+    }
+
+    public Node GetChild(char letterToGet)
+    {
+        // if (!HasChild(letterToGet)) return null;
+        
+        return children.FirstOrDefault(n => n.letter == char.ToUpper(letterToGet));
+    }
+
+    public Node GetOrAddChild(char letterToAdd, bool isEndOfWord = false)
+    {
+        if (HasChild(letterToAdd))
+        {
+            return GetChild(letterToAdd);
+        }
+
+        /*
+        var toAdd = new Node();
+        toAdd.letter = char.ToUpper(letterToAdd);
+        endsValidWord = isEndOfWord;
+        */
+        
+        var toAdd = new Node { letter = char.ToUpper(letterToAdd), endsValidWord = isEndOfWord };
+        
+        children.Add(toAdd);
+
+        return toAdd;
+    }
+    
+    public bool IsWord(string word)
+    {
+        if (word.Length == 0)
+        {
+            return endsValidWord;
+        }
+
+        if (!HasChild(word[0])) 
+            return false;
+
+        return GetChild(word[0]).IsWord(word.Substring(1, word.Length - 1));
+    }
+}
+
